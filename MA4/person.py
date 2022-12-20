8375833bc5b402f5ba1acb19786c574b8d2f020c
Gabrielle Fidelis de Castilho <gafi8647@arrhenius.it.uu.se>
2022-12-20 04:21:47 +0100

    trying the c++ thingy

--- MA4/person.py
+++ MA4/person.py
@@ -1,5 +1,6 @@
 """ Python interface to the C++ Person class """
 import ctypes
+from time import perf_counter as pc
 lib = ctypes.cdll.LoadLibrary('./libperson.so')
 
 class Person(object):
@@ -9,6 +10,8 @@ class Person(object):
 		lib.Person_get.argtypes = [ctypes.c_void_p]
 		lib.Person_get.restype = ctypes.c_int
 		lib.Person_set.argtypes = [ctypes.c_void_p,ctypes.c_int]
+		lib.Person_fib.argtypes = [ctypes.c_void_p,ctypes.c_int]
+		lib.Person_fib.restype = ctypes.c_int
 		lib.Person_delete.argtypes = [ctypes.c_void_p]
 		self.obj = lib.Person_new(age)
 
@@ -17,6 +20,9 @@ class Person(object):
 
 	def set(self, age):
 		lib.Person_set(self.obj, age)
+
+	def fib(self, age):
+		lib.Person_fib(self.obj, age)
         
 	def __del__(self):
 		return lib.Person_delete(self.obj)
